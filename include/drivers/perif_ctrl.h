ed42103d2800127141cffd4d492be5adacabeb06
ZegesMenden <51548005+ZegesMenden@users.noreply.github.com>
2023-02-19 13:52:20 -0500

    move perif_ctrl.h to drivers

--- include/drivers/perif_ctrl.h
+++ include/drivers/perif_ctrl.h
@@ -1,63 +1,11 @@
 #include "core.h"
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
-
-// output drivers
+#include "hardware/spi.h"
+#include "hardware/uart.h"
+#include "hardware/i2c.h"
 #include "drivers/ws2812.pio.h"
 #include "drivers/buzzer.h"
-#include "drivers/pca9685.h"
-
-// datalogging drivers
-#include "drivers/flash.h"
-
-// sensor drivers
-#include "drivers/bmp581.h"
-#include "drivers/lsm6dso32.h"
-#include "drivers/lis2mdl.h"
-
-// state identification
-
-#define pin_neopixel    16
-#define pin_buzzer      10
-
-// pyros
-
-#define pin_pyro_3_cont 18
-#define pin_pyro_3_fire 19
-
-#define pin_pyro_2_cont 20
-#define pin_pyro_2_fire 21
-
-#define pin_pyro_1_cont 22
-#define pin_pyro_1_fire 23
-
-// io
-
-#define pin_qwiic_port0_0 14
-#define pin_qwiic_port0_1 15
-
-#define pin_qwiic_port1_0 13
-#define pin_qwiic_port1_1 12
-
-#define pin_qwiic_port2_0 9
-#define pin_qwiic_port2_1 8
-
-#define pin_qwiic_port3_0 1
-#define pin_qwiic_port3_1 0
-
-#define pin_spi0_clk 2
-#define pin_spi0_sdo 3
-#define pin_spi0_sdi 4
-
-#define pin_cs_mag   5
-#define pin_cs_imu   6
-#define pin_cs_baro  7
-#define pin_cs_flash 7
-#define pin_cs_extra 17
-
-#define pin_adc_pyro   27
-#define pin_adc_switch 28
-#define pin_adc_batt   29
 
 void perif_init() {
 
@@ -148,6 +96,4 @@ void perif_init() {
     buzzer_disable(pin_buzzer);
 
 }
-
-
 
