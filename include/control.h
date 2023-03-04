bd4cd3c0159a42ca9187aff3aa0156afad61369a
ZegesMenden <51548005+ZegesMenden@users.noreply.github.com>
2023-03-04 14:28:58 -0500

    Update control.h

--- include/control.h
+++ include/control.h
@@ -1,2 +1,20 @@
 #include "core.h"
 
+namespace control {
+
+    enum guidance_ctrl_mode_t {
+        ctrl_none,
+        ctrl_ori,
+        ctrl_vel,
+        ctrl_pos_vel,
+    };
+
+    guidance_ctrl_mode_t guidance_ctrl_mode = ctrl_none;
+
+    vec3<float> setpoint_position;
+    vec3<float> setpoint_velocity;
+    vec3<float> setpoint_orientation;
+    
+        
+
+};
